0d300c7f7fc9ef6311372f3a57ef555f4661a03c
LuanGDantas <luan.dantas123@hotmail.com>
2024-05-25 16:30:12 -0300

    refactor: rename error class

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { RegisterUseCase } from '@/use-cases/register';
-import { userAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -18,7 +18,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(prismaUsersRepository);
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
-    if (error instanceof userAlreadyExistsError) {
+    if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message });
     }
 
