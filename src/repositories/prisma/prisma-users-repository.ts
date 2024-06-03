5d41faac1d14792074e89143f5f2c99713efd797
LuanGDantas <luan.dantas123@hotmail.com>
2024-06-03 11:28:10 -0300

    feat: add find user by ID

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,10 @@ import { Prisma } from '@prisma/client';
 import { UsersRepository } from '../user-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    return null;
+  }
+
   async findByEmail(email: string) {
     return prisma.user.findUnique({
       where: {
